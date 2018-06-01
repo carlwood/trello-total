51e877a7c592d8878bd792bbce3b87736bb2eda3
Carl Wood <cjwood17@googlemail.com>
2018-06-01 14:06:00 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@ const $targetSibling = document.querySelector('.board-header-btns.mod-right')
 const classnames = 'board-header-btns mod-left'
 
 function createDiv(count) {
-  let div = document.createElement("div")
-  div.className = classnames
-  div.style.color = 'white'
-  div.innerHTML = count
-  $parent.insertBefore(div, $targetSibling)
+  let $countSpan = document.createElement("div")
+  $countSpan.className = classnames
+  $countSpan.style.color = 'white'
+  $countSpan.innerHTML = count
+  $parent.insertBefore($countSpan, $targetSibling)
 }
 
 window.onload = () => {
